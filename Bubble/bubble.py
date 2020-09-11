2886f2c30b05f54bc091281173e743e0a8d21446
BeckenbauerRijiirkd <beckraika@gmail.com>
2020-09-10 23:51:26 -0300

    Update bubble.py

--- Bubble/bubble.py
+++ Bubble/bubble.py
@@ -1,10 +1,20 @@
 def bubble(vetor):
-    aux = 0 
+    aux = 0 #VARIAVEL AUXILIAR PARA ARMAZENAGEM DO ELEMENTO TEMPORARIAMENTE
+    cont = 0 #VARIAVEL CONTADORA DE VERIFICAÇÕES
     for i in range (0, (len(vetor)-1)): #PRIMEIRO LAÇO PERCORRENDO TOD O VETOR
-        for j in range (0, (len(vetor)-1)): #LAÇO INTERNO DA TROCA OU NAO DE POSIÇÕES.
+        for j in range (0, ((len(vetor)-1)-i)): #LAÇO INTERNO DA TROCA OU NAO DE POSIÇÕES.
+
+            #APRIMORAMENTO DO ALGORITMO, SUBTRAÇÃO DA QUANTIDADE DE PASSAGENS FEITA PELO PRIMEIRO LAÇO
+            #REDUZINDO ASSIM O TOTAL DE VERIFICAÇÕES JA REALIZADAS.
+            
             if vetor[j] > vetor[j+1]: #CONDIÇÃO PARA TROCA DE POSIÇÃO.
                 aux = vetor[j+1] #VARIAVEL AUXILIAR ARMAZENA O ELEMENTO POSTERIOR, IDENTIFICADO COMO MENOR.
                 vetor[j+1] = vetor[j] #ELEMENTO POSTERIOR RECEBE O ELEMENTO ANTERIOR. 
                 vetor[j] = aux # ELEMENTO ANTERIOR RECEBE O VALOR DA VARIAVEL AUXILIAR
-    print(vetor)
-    
+                print(vetor[j+1], "TROCOU COM: ", vetor[j])
+            else:
+                print(vetor[j], "NÃO TROCOU COM: ", vetor[j+1])
+            cont = cont + 1
+            print(vetor,"\n")
+
+    print("VETOR ORDENADO: ", vetor,"\nTOTAL DE VERIFICAÇÕES: ",cont  )
